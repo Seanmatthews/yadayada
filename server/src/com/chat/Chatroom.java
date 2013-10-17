fa89ebda4295e7c302a2246814b5dce138a27ce5
Jim Greco <jgreco@getcollc.com>
2013-10-17 12:28:21 -0400

    Fixed for new project structure

---
+++ server/src/com/chat/Chatroom.java
@@ -0,0 +1,41 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jgreco
+ * Date: 10/17/13
+ * Time: 11:48 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Chatroom {
+    private final Set<User> users = new HashSet<User>();
+
+    public String name;
+    public long id;
+
+    public void addUser(User user) {
+        users.add(user);
+    }
+
+    public void removeUser(User user) {
+        users.remove(user);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (id != user.id) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (id ^ (id >>> 32));
+    }
+}
