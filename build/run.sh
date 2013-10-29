01dd427ab4de352ad8e3213a87656a75d4b699cd
Jim Greco <jgreco@gmail.com>
2013-10-29 17:51:07 -0400

    Added command lines option parser

--- build/run.sh
+++ build/run.sh
@@ -1,1 +1,1 @@
-nohup java -cp "*" com.chat.MainDB 5000 &
+nohup java -cp "*" com.chat.Main -userrepository database -sqluser admin -sqlpassword admin123 -sqlurl jdbc:mysql://userdb01.cehtpxvzecp2.us-west-2.rds.amazonaws.com:3306/userdb -sqldriver com.mysql.jdbc.Driver &
--- server/src/com/chat/Main.java
+++ server/src/com/chat/Main.java
@@ -1,11 +1,16 @@
 package com.chat;
 
+import com.chat.impl.AwsRdsUserRepository;
 import com.chat.server.ChatStreamSocketListener;
 import com.chat.impl.InMemoryChatroomRepository;
 import com.chat.impl.InMemoryMessageRepository;
 import com.chat.impl.InMemoryUserRepository;
+import org.apache.commons.cli.*;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.concurrent.ExecutionException;
 
 /**
@@ -16,17 +21,56 @@ import java.util.concurrent.ExecutionException;
  * To change this template use File | Settings | File Templates.
  */
 public class Main {
-    public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
-        int port = Integer.parseInt(args[0]);
+    public static void main(String[] args) throws IOException, ExecutionException, InterruptedException, SQLException, ClassNotFoundException {
+        CommandLineParser parser = new BasicParser();
+        Options myOptions = new Options();
+        myOptions.addOption("userrepository", true, "memory or database");
+        myOptions.addOption("port", true, "TCP port");
+        myOptions.addOption("sqldriver", true, "Driver for SQL Connection");
+        myOptions.addOption("sqlurl", true, "URL of SQL Server");
+        myOptions.addOption("sqluser", true, "SQL Username");
+        myOptions.addOption("sqlpassword", true, "SQL Username");
+        CommandLine options;
 
-        InMemoryUserRepository userRepo = new InMemoryUserRepository();
-        User admin = userRepo.registerUser("admin", "admin", "admin", "ADMIN_UUID", null).get().getUser();
+        try {
+            options = parser.parse(myOptions, args);
+        } catch (ParseException e) {
+            System.err.println(e.getMessage());
+            return;
+        }
+
+        int port = Integer.parseInt(options.getOptionValue("port", "5000"));
+
+        UserRepository userRepo;
+        User admin;
+
+        String repo = options.getOptionValue("userrepository", "memory");
+        if (repo.equals("database")) {
+            String driver = options.getOptionValue("sqldriver");
+            String connectionString = options.getOptionValue("sqlurl");
+            String username = options.getOptionValue("sqluser");
+            String password = options.getOptionValue("sqlpassword");
+
+            // Setup the connection with the DB
+            Class.forName(driver);
+            Connection connection = DriverManager.getConnection(connectionString, username, password);
+            userRepo = new AwsRdsUserRepository(connection);
+            admin = userRepo.login("admin", "admin", null).get().getUser();
+
+            System.out.println("Loaded database user repository");
+        }
+        else {
+            userRepo = new InMemoryUserRepository();
+            admin = userRepo.registerUser("admin", "admin", "admin", "ADMIN_UUID", null).get().getUser();
+
+            System.out.println("Loaded in-memory user repository");
+        }
 
         InMemoryChatroomRepository chatroomRepo = new InMemoryChatroomRepository();
         chatroomRepo.createChatroom(admin, "Global");
 
-        MessageRepository repo = new InMemoryMessageRepository();
+        MessageRepository messageRepo = new InMemoryMessageRepository();
 
-        new ChatStreamSocketListener(port, userRepo, chatroomRepo, repo);
+        new ChatStreamSocketListener(port, userRepo, chatroomRepo, messageRepo);
     }
 }
