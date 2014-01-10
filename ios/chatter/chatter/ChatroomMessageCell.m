28fe81cca292311b64532dd430e2b097f187aa30
sean matthews <sean.d.matthews@gmail.com>
2014-01-09 21:37:51 -0500

    1) Fixed padding issues in chat view

--- ios/chatter/chatter/ChatroomMessageCell.m
+++ ios/chatter/chatter/ChatroomMessageCell.m
@@ -11,6 +11,7 @@
 @implementation ChatroomMessageCell
 
 const CGFloat BORDER_WIDTH = 2.;
+const CGFloat PADDING = 2.;
 const int WEBVIEW_TAG = 1;
 const int ICONVIEW_TAG = 2;
 const int HIDDEN_VIEW = 3;
@@ -52,9 +53,9 @@ const int HIDDEN_VIEW = 3;
         
         // CSS for table cells
         pageCSS = @"body { margin:0; padding:1; }";
-        cellMsgCSS = @"div.msg { font:12px/13px baskerville,serif; color:#004C3D; text-align:left; vertical-align:text-top; margin:0; padding:0 }";
+        cellMsgCSS = @"div.msg { font:12px/13px baskerville,serif; color:#004C3D; text-align:left; vertical-align:text-top; margin:0; padding:0; word-wrap:break-word }";
         handleCSS = @"div.handle { font:11px/12px baskerville,serif; color:#D0D0D0 }";
-        selfMsgCSS = @"div.msg { font:12px/13px baskerville,serif; color:#004C3D; text-align:right; vertical-align:text-top; margin:0; padding:0 }";
+        selfMsgCSS = @"div.msg { font:12px/13px baskerville,serif; color:#004C3D; text-align:right; vertical-align:text-top; margin:0; padding:0; word-wrap:break-word }";
         selfHandleCSS = @"div.handle { font:11px/12px baskerville,serif; color:#D0D0D0; text-align:right }";
     }
     return self;
@@ -78,16 +79,21 @@ const int HIDDEN_VIEW = 3;
     CGFloat cellWidth = self.frame.size.width;
     CGFloat cellHeight = self.frame.size.height;
     NSString* html;
+    CGFloat heightWithoutBorders = cellHeight-(2*BORDER_WIDTH)-PADDING;
+    CGFloat totalPadding = (2*BORDER_WIDTH) + PADDING;
+    CGFloat iconViewWidth = heightWithoutBorders;
+    CGFloat webViewWidth = cellWidth - iconViewWidth - totalPadding;
     
+    // BORDER_WIDTH's worth of natural padding is included after the first element
     if (_selfMessage) {
-        iconView.frame = CGRectMake(cellWidth-cellHeight, 0, cellHeight, cellHeight);
-        webview.frame = CGRectMake(0, 0, cellWidth-cellHeight, cellHeight);
+        webview.frame = CGRectMake(BORDER_WIDTH, BORDER_WIDTH+PADDING, webViewWidth, heightWithoutBorders);
+        iconView.frame = CGRectMake(webViewWidth+BORDER_WIDTH+PADDING, BORDER_WIDTH+PADDING, iconViewWidth, heightWithoutBorders);
         html = [NSString stringWithFormat:@"<html><head><style> %@ %@ %@ </style></head><body><div class=msg>%@</div><div class=handle>%@</div></body></html>",
                 pageCSS,selfMsgCSS,selfHandleCSS,_message,_userHandle];
     }
     else {
-        iconView.frame = CGRectMake(0, 0, cellHeight, cellHeight);
-        webview.frame = CGRectMake(44, 0, cellWidth-cellHeight, cellHeight);
+        iconView.frame = CGRectMake(BORDER_WIDTH, BORDER_WIDTH+PADDING, iconViewWidth, heightWithoutBorders);
+        webview.frame = CGRectMake(iconViewWidth+BORDER_WIDTH+PADDING, BORDER_WIDTH+PADDING, webViewWidth, heightWithoutBorders);
         html = [NSString stringWithFormat:@"<html><head><style> %@ %@ %@ </style></head><body><div class=msg>%@</div><div class=handle>%@</div></body></html>",
                 pageCSS,cellMsgCSS,handleCSS,_message,_userHandle];
     }
