b78fd2c321ae7314e5d4d5d3a1ddff5dfebc7410
Sean D Matthews <sean@seans-MacBook-Pro.local>
2014-07-15 20:58:31 -0400

    Refactored chat activity to remove some algorithmic errors (#7)

--- java/src/com/chat/impl/LCMVChatroomActivity.java
+++ java/src/com/chat/impl/LCMVChatroomActivity.java
@@ -6,12 +6,18 @@ import java.util.Timer;
 
 /**
  * Created by sean on 6/25/14.
+ *
+ * This class implements the ChatroomActivity interface to provide a chatroom activity number, which is
+ * representative of how active a particular chatroom is at any one instant.
+ *
+ * How it works:
+ * The initial chat activity value starts at 0%.
+ *
  */
 public class LCMVChatroomActivity implements ChatroomActivity {
 
     private long creationTime;
     private long messageGroupStartTime;
-    private long messageCount;
     private long currentGroupMessageCount;
     private long lastMessageTimestamp;
     private double instantaneousActivity;
@@ -24,47 +30,53 @@ public class LCMVChatroomActivity implements ChatroomActivity {
         NEW_GROUP_INTERVAL = 15;
         creationTime = System.currentTimeMillis() / 1000L;
         messageGroupStartTime = creationTime;
-        activity = 0;
+        activity = 0.99; // start at 100%
         instantaneousActivity = 0;
-        seedGroupMessageRate = 1.;
+        seedGroupMessageRate = 1.; // 1 per second
+        lastMessageTimestamp = creationTime; // faux first message
+        currentGroupMessageCount = 0;
     }
 
     // Add a new message and update the chatroom activity
-    // Returns: current chatroom activity
+    // Returns: current chatroom activity as percentage
     @Override
     public short newMessage(String message) {
-        lastMessageTimestamp = System.currentTimeMillis() / 1000L;
-        messageCount++;
-
         if (isNewGroup()) {
             // A new group is starting, update based on last group
             updateActivity();
             messageGroupStartTime = System.currentTimeMillis() / 1000L;
+            seedGroupMessageRate = (double)currentGroupMessageCount / (double)messageGroupAge();
+            currentGroupMessageCount = 0;
         }
-        else {
-            currentGroupMessageCount++;
-        }
 
-        return activity < 1. ? (short)(activity * 100.) : 100;
-    }
+        currentGroupMessageCount++;
 
-    @Override
-    public short getActivityPercentage() {
-        return activity < 1. ? (short)(activity * 100.) : 100;
-    }
-
-    private void updateActivity() {
-        // message rate over last group
+        // group rate versus seed rate --
+        // this will be the current group
         double messageRate = (double)currentGroupMessageCount / (double)messageGroupAge();
-
-        // group rate versus seed rate
         instantaneousActivity = messageRate / seedGroupMessageRate;
 
-        // set new seed rate to last group rate
-        seedGroupMessageRate = messageRate;
+        lastMessageTimestamp = System.currentTimeMillis() / 1000L;
 
+        return getActivityPercentage();
+    }
+
+    // Return the current chatroom activity as percentage
+    @Override
+    public short getActivityPercentage() {
+        return (short)((activity < 1. ? activity : 1.) * 100.);
+    }
+
+    // Update chat activity
+    private void updateActivity() {
         // take average of new activity measure and the average of the all old ones
-        activity = (activity + instantaneousActivity) / 2.;
+        // Algorithm:
+        // (0.5 * prior m/s + 0.5 * current m/s) * 100
+        // where current m/s = num messages this group / group duration / last group message rate
+        // where prior m/s = last getActivityPercentage()
+        double priorActivity = activity < 1. ? activity : 1.;
+        double currentActivity = instantaneousActivity < 1. ? instantaneousActivity : 1.;
+        activity = (priorActivity + currentActivity) / 2.;
     }
 
     // Determine whether a sufficient time interval has passed to start a new group
